d8359d8dc0819e1bf99d5c540bdef03fc01eb7b0
Kirsten Bark <kirsten.bark@quantumsimulations.de>
2021-07-08 15:37:07 +0200

    Changed qoqo calculator pyo3 version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ __version__ = lines[-1].strip().split("'")[1].strip()
 
 install_requires = [
     'myqlm; python_version != "3.7.*,3.9.*"',
-    'qoqo_calculator_pyo3>=0.3.0',
+    'qoqo_calculator_pyo3>=0.2.0',
     'qoqo>=0.4.0',
     'numpy',
 ]
